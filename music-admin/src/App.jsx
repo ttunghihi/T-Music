f0dfa1c4f9f7e0510b029fa39433ebc9dc993fec
ttunghihi <mrdevil6423@gmail.com>
2025-12-07 03:47:54 +0700

    Update App.jsx

--- music-admin/src/App.jsx
+++ music-admin/src/App.jsx
@@ -3,7 +3,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Routes, Route } from "react-router-dom";
 import AddSong from "./pages/AddSong";
-import AddAlbum from "./pages/addAlbum";
+import AddAlbum from "./pages/AddAlbum";
 import ListSong from "./pages/ListSong";
 import ListAlbum from "./pages/ListAlbum";
 import Sidebar from "./components/SideBar";
