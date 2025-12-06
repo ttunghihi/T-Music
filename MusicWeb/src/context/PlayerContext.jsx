89ba091fe2d6c1e96c4e156a4e022a303e0878e7
ttunghihi <mrdevil6423@gmail.com>
2025-12-07 03:22:02 +0700

    Update audio URL to production server

--- MusicWeb/src/context/PlayerContext.jsx
+++ MusicWeb/src/context/PlayerContext.jsx
@@ -8,7 +8,7 @@ const PlayerContextProvider = (props) => {
   const seekBg = useRef();
   const seekBar = useRef();
 
-  const url = "http://localhost:4000";
+  const url = "https://t-music.onrender.com";
 
   const [songsData, setSongsData] = useState([]);
   const [albumsData, setAlbumsData] = useState([]);
