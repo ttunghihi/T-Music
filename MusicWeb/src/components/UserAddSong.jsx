81d849cb4d298e6faa46d592f742a19f08e5d10b
ttunghihi <mrdevil6423@gmail.com>
2025-12-07 03:21:29 +0700

    Change API base URL to production endpoint

--- MusicWeb/src/components/UserAddSong.jsx
+++ MusicWeb/src/components/UserAddSong.jsx
@@ -5,7 +5,7 @@ import { assets } from "../assets/assets";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_BASE = "http://localhost:4000"; // nếu khác thì đổi ở đây
+const API_BASE = "https://t-music.onrender.com"; // nếu khác thì đổi ở đây
 
 const UserAddSong = () => {
   const navigate = useNavigate();
